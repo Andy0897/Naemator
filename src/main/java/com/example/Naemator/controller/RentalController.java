6fedba046870af91d31c49c9150c6cbf61e62c9a
Kristiyangg <ganchevkristian@gmail.com>
2026-01-22 14:24:40 +0200

    Commit

---
+++ src/main/java/com/example/Naemator/controller/RentalController.java
@@ -0,0 +1,10 @@
+package com.example.Naemator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rentals")
+public class RentalController {
+
+}
---
+++ src/main/java/com/example/Naemator/repository/RentalRepository.java
@@ -0,0 +1,7 @@
+package com.example.Naemator.repository;
+
+import com.example.Naemator.model.Rental;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RentalRepository extends CrudRepository<Rental, Long> {
+}
